5300c69c4212252461250166ec12465f31bbd3a9
Brad Wilson <brad@lucky-dip.net>
2014-10-14 18:13:54 +1100

    add optional http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  if ENV["HTTP_AUTH_NAME"].present?
+    http_basic_authenticate_with name: ENV["HTTP_AUTH_NAME"],
+                                 password: ENV["HTTP_AUTH_PASSWORD"]
+  end
 end
